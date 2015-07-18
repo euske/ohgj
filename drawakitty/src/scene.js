f24e8a93bb9a224f5892c4800ccb4d653dda7f9e
Yusuke Shinyama <yusuke@shinyama.jp>
2015-07-19 08:28:16 +0900

    Added: drawakitty

---
+++ drawakitty/src/scene.js
@@ -0,0 +1,247 @@
+// scene.js
+// Scene object takes care of every in-game object and the scrollable map.
+
+function Scene(game)
+{
+  this.game = game;
+  this.music = null;
+  this.changed = new Slot(this);
+}
+
+Scene.prototype.init = function ()
+{
+};
+
+Scene.prototype.update = function ()
+{
+};
+
+Scene.prototype.render = function (ctx, bx, by)
+{
+  // Fill with the background color.
+  ctx.fillStyle = 'rgb(0,0,0)';
+  ctx.fillRect(bx, by, this.game.screen.width, this.game.screen.height);
+};
+
+Scene.prototype.move = function (vx, vy)
+{
+};
+
+Scene.prototype.action = function (action)
+{
+};
+
+Scene.prototype.mousedown = function (x, y, button)
+{
+};
+
+Scene.prototype.mouseup = function (x, y, button)
+{
+};
+
+Scene.prototype.mousemove = function (x, y)
+{
+};
+
+
+//  Title
+//
+function Title(game)
+{
+  Scene.call(this, game);
+}
+
+Title.prototype = Object.create(Scene.prototype);
+
+Title.prototype.init = function (text)
+{
+  var frame = this.game.frame;
+  var e = this.game.addElement(
+    new Rectangle(frame.width/8, frame.height/4,
+		  3*frame.width/4, frame.height/2));
+  e.align = 'left';
+  e.style.padding = '10px';
+  e.style.color = 'black';
+  e.style.background = 'white';
+  e.style.border = 'solid black 2px';
+  e.innerHTML = text;
+  var changed = this.changed;
+  e.onmousedown = (function (e) { changed.signal(); });
+};
+
+Title.prototype.mousedown = function (x, y, button)
+{
+  this.changed.signal();
+}
+
+Title.prototype.action = function (action)
+{
+  if (action) {
+    this.changed.signal();
+  }
+};
+
+
+//  Level
+// 
+function Level(game)
+{
+  Scene.call(this, game);
+  this.buf = createCanvas(game.screen.width,
+			  game.screen.height);
+  this.ctx = getEdgeyContext(this.buf)
+}
+
+Level.prototype = Object.create(Scene.prototype);
+  
+Level.prototype.init = function ()
+{
+  // [OVERRIDE]
+  this.tasks = [];
+  this.sprites = [];
+  this.colliders = [];
+  this.ticks = 0;
+  this.start = false;
+  
+  this.ctx.fillStyle = 'white';
+  this.ctx.fillRect(0, 0, this.buf.width, this.buf.height);
+  this.x0 = 0;
+  this.y0 = 0;
+  this.mouse = false;
+};
+
+Level.prototype.update = function ()
+{
+  // [OVERRIDE]
+  this.updateObjects(this.tasks);
+  this.cleanObjects(this.tasks);
+  this.cleanObjects(this.sprites);
+  this.cleanObjects(this.colliders);
+  this.ticks++;
+  
+  for (var i = 0; i < this.colliders.length; i++) {
+    var obj1 = this.colliders[i];
+    if (obj1.alive && obj1.scene === this && obj1.bounds !== null && this.mouse) {
+      if (obj1.bounds.contains(this.x0, this.y0) ||
+	  obj1.bounds.move(obj1.vx, 0).contains(this.x0, this.y0)) {
+	this.changed.signal('LOST', 0);
+      }
+    }
+  }
+
+  if (this.start && rnd(10) == 0) {
+    var vx = rnd(20)-10;
+    var s = rnd(20)+10;
+    if (vx != 0) {
+      var bounds = new Rectangle((0 < vx)? 0 : this.buf.width-s, rnd(this.buf.height-s), s, s);
+      this.addObject(new Actor(bounds, vx));
+    }
+  }
+
+};
+
+Level.prototype.render = function (ctx, bx, by)
+{
+  // [OVERRIDE]
+  Scene.prototype.render.call(this, ctx, bx, by);
+
+  ctx.drawImage(this.buf, bx, by);
+  
+  // Draw the sprites.
+  for (var i = 0; i < this.sprites.length; i++) {
+    var obj = this.sprites[i];
+    if (obj.scene !== this) continue;
+    var b = obj.bounds;
+    if (b !== null) {
+      obj.render(ctx, bx+b.x, by+b.y);
+    } else {
+      obj.render(ctx, bx, by);
+    }
+  }
+};
+
+Level.prototype.collide = function (obj0)
+{
+  var a = [];
+  if (obj0.alive && obj0.scene === this && obj0.hitbox !== null) {
+    for (var i = 0; i < this.colliders.length; i++) {
+      var obj1 = this.colliders[i];
+      if (obj1.alive && obj1.scene === this && obj1.hitbox !== null &&
+	  obj1 !== obj0 && obj1.hitbox.overlap(obj0.hitbox)) {
+	a.push(obj1);
+      }
+    }
+  }
+  return a;
+};
+  
+Level.prototype.addObject = function (obj)
+{
+  if (obj.update !== undefined) {
+    if (obj.scene === null) {
+      obj.start(this);
+    }
+    this.tasks.push(obj);
+  }
+  if (obj.render !== undefined) {
+    this.sprites.push(obj);
+  }
+  if (obj.hitbox !== undefined) {
+    this.colliders.push(obj);
+  }
+};
+
+Level.prototype.removeObject = function (obj)
+{
+  if (obj.update !== undefined) {
+    removeArray(this.tasks, obj);
+  }
+  if (obj.render !== undefined) {
+    removeArray(this.sprites, obj);
+  }
+  if (obj.hitbox !== undefined) {
+    removeArray(this.colliders, obj);
+  }
+};
+
+Level.prototype.updateObjects = function (objs)
+{
+  for (var i = 0; i < objs.length; i++) {
+    objs[i].update();
+  }
+}
+
+Level.prototype.cleanObjects = function (objs)
+{
+  function f(obj) { return !obj.alive; }
+  removeArray(objs, f);
+}
+
+Level.prototype.mousedown = function (x, y, button)
+{
+  // [OVERRIDE]
+  this.start = true;
+  this.x0 = x;
+  this.y0 = y;
+  this.mouse = true;
+};
+
+Level.prototype.mouseup = function (x, y, button)
+{
+  // [OVERRIDE]
+  this.mouse = false;
+};
+
+Level.prototype.mousemove = function (x, y)
+{
+  // [OVERRIDE]
+  if (this.mouse) {
+    this.ctx.strokeStyle = 'black';
+    this.ctx.beginPath();
+    this.ctx.moveTo(this.x0, this.y0);
+    this.ctx.lineTo(x, y);
+    this.ctx.stroke();
+    this.x0 = x;
+    this.y0 = y;
+  }
+};
