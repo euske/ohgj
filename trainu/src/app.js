5e73c9f41430d3723a0b45d364dfef64bf4cf795
Yusuke Shinyama <yusuke@shinyama.jp>
2016-02-18 00:12:48 +0900

    Added: trainu

---
+++ trainu/src/app.js
@@ -0,0 +1,244 @@
+// app.js
+//   requires: utils.js
+//   requires: geom.js
+//   requires: scene.js
+//   requires: text.js
+'use strict';
+
+// App class handles the event loop and global state management.
+// It also has shared resources (images, audios, etc.)
+
+function App(framerate, frame, images, audios, labels)
+{
+  this.framerate = framerate;
+  this.frame = frame;
+  this.images = images;
+  this.audios = audios;
+  this.labels = labels;
+  this.active = false;
+  this.msgs = [];
+  this.music = null;
+
+  // Initialize the off-screen bitmap.
+  var scale = 4;
+  this.screen = createCanvas(this.frame.width/scale,
+			     this.frame.height/scale);
+  this.ctx = getEdgeyContext(this.screen);
+
+  // [GAME SPECIFIC CODE]
+  this.sprites = this.images.sprites;
+  this.sprites_size = new Vec2(this.sprites.height, this.sprites.height);
+  this.font = new Font(this.images.font, 'white');
+  this.colorfont = new Font(this.images.font);
+  
+  this._key_left = false;
+  this._key_right = false;
+  this._key_up = false;
+  this._key_down = false;
+  this.key_action = false;
+  this.key_dir = new Vec2();
+  this.keylock = 0;
+}
+
+define(App, Object, '', {
+  addElement: function (bounds) {
+    var e = document.createElement('div');
+    e.style.position = 'absolute';
+    e.style.left = bounds.x+'px';
+    e.style.top = bounds.y+'px';
+    e.style.width = bounds.width+'px';
+    e.style.height = bounds.height+'px';
+    e.style.padding = '0px';
+    this.frame.parentNode.appendChild(e);
+    return e;
+  },
+
+  removeElement: function (e) {
+    e.parentNode.removeChild(e);
+  },
+
+  lockKeys: function () {
+    this.keylock = this.framerate;
+  },
+
+  keydown: function (ev) {
+    if (0 < this.keylock) return;
+    // [OVERRIDE]
+    // [GAME SPECIFIC CODE]
+    var keysym = getKeySym(ev.keyCode);
+    switch (keysym) {
+    case 'left':
+      this._key_left = true;
+      this.key_dir.x = -1;
+      this.scene.set_dir(this.key_dir.x, 0);
+      break;
+    case 'right':
+      this._key_right = true;
+      this.key_dir.x = +1;
+      this.scene.set_dir(this.key_dir.x, 0);
+      break;
+    case 'up':
+      this._key_up = true;
+      this.key_dir.y = -1;
+      this.scene.set_dir(0, this.key_dir.y);
+      break;
+    case 'down':
+      this._key_down = true;
+      this.key_dir.y = +1;
+      this.scene.set_dir(0, this.key_dir.y);
+      break;
+    case 'action':
+      if (!this.key_action) {
+	this.key_action = true;
+	this.scene.set_action(this.key_action);
+      }
+      break;
+    default:
+      switch (ev.keyCode) {
+      case 112:			// F1
+	break;
+      case 27:			// ESC
+	if (this.active) {
+	  this.blur();
+	} else {
+	  this.focus();
+	}
+	break;
+      }
+      break;
+    }
+    this.scene.keydown(ev.keyCode);
+  },
+
+  keyup: function (ev) {
+    // [OVERRIDE]
+    // [GAME SPECIFIC CODE]
+    var keysym = getKeySym(ev.keyCode);
+    switch (keysym) {
+    case 'left':
+      this._key_left = false;
+      this.key_dir.x = (this._key_right) ? +1 : 0;
+      this.scene.set_dir(this.key_dir.x, 0);
+      break;
+    case 'right':
+      this._key_right = false;
+      this.key_dir.x = (this._key_left) ? -1 : 0;
+      this.scene.set_dir(this.key_dir.x, 0);
+      break;
+    case 'up':
+      this._key_up = false;
+      this.key_dir.y = (this._key_down) ? +1 : 0;
+      this.scene.set_dir(0, this.key_dir.y);
+      break;
+    case 'down':
+      this._key_down = false;
+      this.key_dir.y = (this._key_up) ? -1 : 0;
+      this.scene.set_dir(0, this.key_dir.y);
+      break;
+    case 'action':
+      if (this.key_action) {
+	this.key_action = false;
+	this.scene.set_action(this.key_action);
+      }
+      break;
+    }
+    this.scene.keyup(ev.keyCode);
+  },
+
+  mousedown: function (ev) {
+    // [OVERRIDE]
+    if (this.scene.mousedown !== undefined) {
+      if (ev.target === this.frame) {
+	this.scene.mousedown(
+	  ev.layerX*this.screen.width/this.frame.width,
+	  ev.layerY*this.screen.height/this.frame.height,
+	  ev.button);
+      }
+    }
+  },
+
+  mouseup: function (ev) {
+    // [OVERRIDE]
+    if (this.scene.mouseup !== undefined) {
+      if (ev.target === this.frame) {
+	this.scene.mouseup(
+	  ev.layerX*this.screen.width/this.frame.width,
+	  ev.layerY*this.screen.height/this.frame.height,
+	  ev.button);
+      }
+    }
+  },
+
+  mousemove: function (ev) {
+    // [OVERRIDE]
+    if (this.scene.mousemove !== undefined) {
+      if (ev.target === this.frame) {
+	this.scene.mousemove(
+	  ev.layerX*this.screen.width/this.frame.width,
+	  ev.layerY*this.screen.height/this.frame.height);
+      }
+    }
+  },
+
+  focus: function (ev) {
+    // [OVERRIDE]
+    this.active = true;
+    if (this.music !== null && this.music.loop) {
+      this.music.play();
+    }
+  },
+
+  blur: function (ev) {
+    // [OVERRIDE]
+    if (this.music !== null) {
+      this.music.pause();
+    }
+    this.active = false;
+  },
+
+  init: function (scene) {
+    // [OVERRIDE]
+    removeChildren(this.frame.parentNode, 'div');
+
+    this.set_music(null);
+    this.scene = scene;
+    this.scene.init();
+  },
+
+  set_music: function (music) {
+    if (this.music !== null) {
+      this.music.pause();
+    }
+    this.music = music;
+    if (this.music !== null) {
+      this.music.play();
+    }
+  },
+  
+  post: function (msg) {
+    this.msgs.push(msg);
+  },
+
+  tick: function () {
+    // [OVERRIDE]
+    // [GAME SPECIFIC CODE]
+    this.scene.tick();
+    if (0 < this.keylock) {
+      this.keylock--;
+    }
+
+    while (0 < this.msgs.length) {
+      var msg = this.msgs.shift();
+      msg();
+    }
+  },
+
+  repaint: function () {
+    // [OVERRIDE]
+    this.ctx.clearRect(0, 0, this.screen.width, this.screen.height);
+    this.ctx.save();
+    this.scene.render(this.ctx, 0, 0);
+    this.ctx.restore();
+  },
+
+});
---
+++ trainu/src/game.js
@@ -0,0 +1,167 @@
+// game.js
+//   requires: utils.js
+//   requires: geom.js
+//   requires: actor.js
+//   requires: scene.js
+//   requires: text.js
+//   requires: tilemap.js
+//   requires: app.js
+'use strict';
+
+
+// Block
+function Block(bounds)
+{
+  this._Actor(bounds, bounds.inflate(-8, 0), 2);
+  this.movement = new Vec2(-4, 0);
+}
+
+define(Block, Actor, 'Actor', {
+  update: function () {
+    this.move(this.movement);
+    if (this.bounds.right() <= 0) {
+      this.die();
+    }
+  },
+});
+
+// Train
+function Train(bounds, scene, color)
+{
+  this._PhysicalActor(bounds, bounds.inflate(-8, 0), 0);
+  this.jumpfunc = (function (vy, t) {
+    if (0 <= t && t < 8) return -2;
+    if (t < 20) return vy;
+    return vy+1;
+  });
+  this.zorder = 1;
+  this.scene = scene;
+  this.prev = null;
+  this.next = null;
+  this.speed = rnd(4, 8);
+}
+
+define(Train, PhysicalActor, 'PhysicalActor', {
+  getContactFor: function (v, hitbox, range) {
+    return hitbox.contact(v, this.scene.ground);
+  },
+
+  collide: function (obj) {
+    if (obj instanceof Block) {
+      playSound(this.scene.app.audios.hurt);
+    }
+  },
+
+  update: function () {
+    if (this.prev !== null && this.next !== null) {
+      var v = this.velocity.copy();
+      v.y = (this.prev.velocity.y + this.next.velocity.y)/2;
+      this.velocity = this.getMove(v, this.hitbox, false);
+      this.move(this.velocity);
+    } else {
+      this._PhysicalActor_update();
+    }
+    this.tileno = ((1 < Math.abs(this.velocity.y))? 1 :
+		   (blink(this.getTime(), this.speed)? 1 : 0));
+  },
+
+  setJump: function (jumpend) {
+    this._PhysicalActor_setJump(jumpend);
+    if (0 < jumpend && this._jumpt == 0) {
+      playSound(this.scene.app.audios.jump);
+    }
+  },
+
+});
+
+
+//  Game
+// 
+function Game(app)
+{
+  this._GameScene(app);
+}
+
+define(Game, GameScene, 'GameScene', {
+  init: function () {
+    this._GameScene_init();
+
+    var app = this.app;
+
+    this.background2 = new TileSprite(this.screen, this.app.images.background2);
+    this.addObject(this.background2);
+    this.background1 = new TileSprite(this.screen, this.app.images.background1);
+    this.addObject(this.background1);
+    
+    this.ground = new Rectangle(0, this.screen.height-10, this.screen.width, 10);
+    var N = 4;
+    var x0 = (N-1)*30;
+    var y0 = this.ground.y;
+    var prev = null;
+    this.first = null;
+    this.last = null;
+    for (var i = 0; i < N; i++) {
+      var color = 'white';
+      if (i == 0) {
+	color = 'red';
+      } else if (i == N-1) {
+	color = 'blue';
+      }
+      var obj = new Train(new Rectangle(x0-i*30, y0-32, 32, 32), this, color);
+      if (prev !== null) {
+	prev.next = obj;
+      }
+      obj.prev = prev;
+      prev = obj;
+      this.addObject(obj);
+      if (this.first === null) {
+	this.first = obj;
+      }
+      this.last = obj;
+    }
+    
+    this.nextadd = 60;
+  },
+
+  render: function (ctx, bx, by) {
+    ctx.fillStyle = 'rgb(0,0,0)';
+    ctx.fillRect(bx, by, this.screen.width, this.screen.height);
+    this._GameScene_render(ctx, bx, by);
+  },
+
+  tick: function () {
+    this._GameScene_tick();
+    this.background1.offset.x += 2;
+    this.background2.offset.x += 1;
+    if (this.nextadd < this.layer.ticks) {
+      var rect = new Rectangle(this.screen.width, this.ground.y-20, 32, 20);
+      this.addObject(new Block(rect));
+      this.nextadd = this.layer.ticks + rnd(60, 120);
+    }
+  },
+
+  keydown: function (key) {
+    this._GameScene_keydown(key);
+    switch (getKeySym(key)) {
+    case 'left':
+      this.last.setJump(Infinity);
+      break;
+    case 'right':
+      this.first.setJump(Infinity);
+      break;
+    }
+  },
+  
+  keyup: function (key) {
+    this._GameScene_keyup(key);
+    switch (getKeySym(key)) {
+    case 'left':
+      this.last.setJump(0);
+      break;
+    case 'right':
+      this.first.setJump(0);
+      break;
+    }
+  },
+
+});
