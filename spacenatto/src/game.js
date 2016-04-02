dff68b3b57975dead81be20274669010ec0d2fa6
Yusuke Shinyama <yusuke@shinyama.jp>
2016-04-03 07:30:40 +0900

    Added: spacenatto.

---
+++ spacenatto/src/game.js
@@ -0,0 +1,269 @@
+// game.js
+//   requires: utils.js
+//   requires: geom.js
+//   requires: actor.js
+//   requires: scene.js
+//   requires: text.js
+//   requires: app.js
+'use strict';
+
+
+// Thingy
+function Thingy(scene, bounds, movement)
+{
+  this._Actor(bounds, bounds, '#ff0000');
+  this.scene = scene;
+  this.stuck = false;
+  this.movement = movement;
+}
+
+define(Thingy, Actor, 'Actor', {
+  update: function () {
+    this._Actor_update();
+    if (!this.scene.screen.overlap(this.hitbox)) {
+      this.die();
+    }
+    if (!this.stuck && this.scene.ground.overlap(this.hitbox)) {
+      this.stick();
+    }
+  },
+
+  stick: function () {
+    this.stuck = true;
+    this.movement = new Vec2();
+    this.tileno = '#800000';
+  },
+
+});
+
+// Player
+function Player(scene, bounds)
+{
+  this._PhysicalActor(bounds, bounds, '#00ff00');
+  this.jumpfunc = (function (vy, t) { return (0 <= t && t <= 4)? -8 : vy+2; });
+  this.speed = 4;
+  this.scene = scene;
+}
+
+define(Player, PhysicalActor, 'PhysicalActor', {
+  setMove: function (v) {
+    this.movement.x = v.x*this.speed;
+  },
+
+  fall: function () {
+    if (!this.isHolding()) {
+      var vy = this.jumpfunc(this.velocity.y, this._jumpt);
+      this.velocity.y = vy;
+      this.velocity = this.getMove(this.velocity, this.hitbox, false);
+      if (0 < vy && this.velocity.y == 0) {
+	this.scene.land();
+      }
+      this.move(this.velocity);
+    }
+  },
+
+  collide: function (actor) {
+    if (actor instanceof Thingy && !actor.stuck) {
+      this.scene.bombed();
+      this.die();
+    }
+  },
+  
+  getConstraintsFor: function (hitbox, force) {
+    return this.scene.screen;
+  },
+  
+  getContactFor: function (v, hitbox, force, range) {
+    function isThingy(actor) {
+      return actor instanceof Thingy;
+    }
+    var a = this.layer.findObjects(range, isThingy);
+    for (var i = 0; i < a.length; i++) {
+      var obj = a[i];
+      if (obj.stuck && !obj.hitbox.overlap(hitbox)) {
+	v = hitbox.contact(v, obj.hitbox);
+      }
+    }
+    return hitbox.contact(v, this.scene.ground);
+  },
+
+});
+
+
+//  Stars
+//
+function Stars(frame, nstars, color, speed, maxdepth)
+{
+  this._Layer();
+  this.frame = frame;
+  this.color = (color !== undefined)? color : 'white';
+  this.speed = (speed !== undefined)? speed : -10;
+  this.maxdepth = (maxdepth !== undefined)? maxdepth : 3;
+  this.objs = [];
+  for (var i = 0; i < nstars; i++) {
+    this.objs.push(this.initStar({}));
+  }
+}
+define(Stars, Layer, 'Layer', {
+  initStar: function (obj) {
+    obj.s = Math.random()*2+1;
+    obj.z = Math.random()*this.maxdepth+1;
+    obj.x = rnd(this.frame.width);
+    obj.y = rnd(this.frame.height);
+    return obj;
+  },
+
+  tick: function () {
+    this._Layer_tick();
+    for (var i = 0; i < this.objs.length; i++) {
+      var obj = this.objs[i];
+      obj.y += this.speed/obj.z;
+      if (obj.y+(obj.s/obj.z) < 0) {
+	this.initStar(obj);
+	obj.y = this.frame.width;
+      } else if (this.frame.width < obj.x) {
+	this.initStar(obj);
+	obj.x = 0;
+      }
+    }
+  },
+
+  render: function (ctx, bx, by) {
+    ctx.fillStyle = this.color;
+    bx += this.frame.x;
+    by += this.frame.y;
+    for (var i = 0; i < this.objs.length; i++) {
+      var obj = this.objs[i];
+      var s = obj.s/obj.z;
+      ctx.fillRect(bx+obj.x, by+obj.y, s, s);
+    }
+  },
+});
+
+
+//  Game
+// 
+function Game(app)
+{
+  this._GameScene(app);
+  this.highscore = 0;
+}
+
+define(Game, GameScene, 'GameScene', {
+  init: function () {
+    this._GameScene_init();
+
+    var app = this.app;
+    this.ground = new Rectangle(0, this.screen.height-16, this.screen.width, 16);
+    this.player = new Player(this, new Rectangle(this.screen.width/2,0,8,8));
+    this.addObject(this.player);
+    this.y0 = this.ground.y-this.player.hitbox.height;
+
+    this.nextadd = 30;
+
+    this.stars = new Stars(this.screen, 100);
+    
+    // show a banner.
+    var scene = this;
+    var tb = new TextBox(this.screen, app.font);
+    tb.putText(['GO HIGHER!!1'], 'center', 'center');
+    tb.duration = app.framerate*2;
+    tb.update = function () {
+      tb.visible = blink(tb.layer.ticks, app.framerate/2);
+    };
+    this.addObject(tb);
+
+    this.text_score = new TextBox(this.screen.inflate(-4,-4), app.font);
+    this.addObject(this.text_score);
+    this.update_score();
+
+    playSound(app.audios.start);
+  },
+
+  tick: function () {
+    this._GameScene_tick();
+    this.stars.tick();
+
+    var layer = this.layer;
+    var objs = [];
+    for (var i = 0; i < layer.colliders.length; i++) {
+      var obj = layer.colliders[i];
+      if (obj.layer === layer && obj instanceof Thingy) {
+	objs.push(obj);
+      }
+    }
+    for (var i = 0; i < objs.length; i++) {
+      var obj0 = objs[i];
+      var hitbox0 = obj0.hitbox.union(obj0.hitbox.add(obj0.movement));
+      for (var j = i+1; j < objs.length; j++) {
+	var obj1 = objs[j];
+	var hitbox1 = obj1.hitbox.union(obj1.hitbox.add(obj1.movement));
+	if (hitbox0.overlap(hitbox1)) {
+	  obj0.stick();
+	  obj1.stick();
+	}
+      }
+    }
+    
+    if (this.nextadd < this.layer.ticks) {
+      var x = this.player.hitbox.x+rnd(40)-20;
+      if (0 <= x && x < this.screen.width-8) {
+	var rect = new Rectangle(x, 0, 8, 8);
+	var v = new Vec2(rnd(3)-1, 2);
+	this.addObject(new Thingy(this, rect, v));
+      }
+      this.nextadd = this.layer.ticks + rnd(5, 10);
+    }
+  },
+
+  land: function () {
+    var score = this.y0 - this.player.bounds.y;
+    if (this.highscore < score) {
+      playSound(this.app.audios.beep);
+      this.highscore = score;
+      this.update_score();
+    }
+  },
+
+  update_score: function () {
+    this.text_score.clear();
+    this.text_score.putText(['HISCORE:'+format(this.highscore)], 'right');
+  },
+
+  render: function (ctx, bx, by) {
+    ctx.fillStyle = 'rgb(0,0,0)';
+    ctx.fillRect(bx, by, this.screen.width, this.screen.height);
+    this.stars.render(ctx, bx, by);
+    ctx.fillStyle = 'rgb(128,128,128)';
+    ctx.fillRect(bx+this.ground.x, by+this.ground.y,
+		 this.ground.width, this.ground.height);
+    this._GameScene_render(ctx, bx, by);
+  },
+
+  set_dir: function (vx, vy) {
+    this._GameScene_set_dir(vx, vy);
+    this.player.setMove(new Vec2(vx, vy));
+  },
+
+  set_action: function (action) {
+    this._GameScene_set_action(action);
+    if (action) {
+      playSound(this.app.audios.jump);
+    }
+    this.player.setJump(action? Infinity : 0);
+  },
+
+  bombed: function () {
+    var scene = this;
+    var app = this.app;
+    playSound(app.audios.hurt);
+    var tb = new TextBox(this.screen, app.font);
+    tb.putText(['DEAD.'], 'center', 'center');
+    tb.duration = app.framerate;
+    tb.died.subscribe(function (_) {
+      scene.init();
+    });
+    scene.addObject(tb);
+  },
+
+});
---
+++ spacenatto/src/geom.js
@@ -0,0 +1,545 @@
+// geom.js
+// Geometric functions.
+//   requires: utils.js
+'use strict';
+
+// Vec2
+function Vec2(x, y)
+{
+  this.x = (x !== undefined)? x : 0;
+  this.y = (y !== undefined)? y : 0;
+}
+var Point = Vec2;
+define(Vec2, Object, '', {
+  toString: function () {
+    return '('+this.x+', '+this.y+')';
+  },
+  equals: function (p) {
+    return (this.x == p.x && this.y == p.y);
+  },
+  isZero: function () {
+    return (this.x == 0 && this.y == 0);
+  },
+  copy: function () {
+    return new Vec2(this.x, this.y);
+  },
+  norm2: function () {
+    return (this.x*this.x + this.y*this.y);
+  },
+  norm: function () {
+    return Math.sqrt(this.x*this.x + this.y*this.y);
+  },
+  sign: function () {
+    return new Vec2(sign(this.x), sign(this.y));
+  },
+  add: function (v) {
+    return new Vec2(this.x+v.x, this.y+v.y);
+  },
+  sub: function (v) {
+    return new Vec2(this.x-v.x, this.y-v.y);
+  },
+  scale: function (v) {
+    return new Vec2(this.x*v, this.y*v);
+  },
+  distance: function (v) {
+    return this.sub(v).norm();
+  },
+  clamp: function (v) {
+    return new Vec2(
+      clamp(-v.x, this.x, +v.x),
+      clamp(-v.y, this.y, +v.y));
+  },
+  move: function (dx, dy) {
+    return new Vec2(this.x+dx, this.y+dy);
+  },
+  rot90: function (v) {
+    if (v < 0) {
+      return new Vec2(this.y, -this.x);
+    } else if (0 < v) {
+      return new Vec2(-this.y, this.x);
+    } else {
+      return this.copy();
+    }
+  },
+  expand: function (dw, dh, vx, vy) {
+    return new Rectangle(this.x, this.y).expand(dw, dh, vx, vy);
+  },
+});
+
+
+// Vec3
+function Vec3(x, y, z)
+{
+  this.x = (x !== undefined)? x : 0;
+  this.y = (y !== undefined)? y : 0;
+  this.z = (z !== undefined)? z : 0;
+}
+define(Vec3, Object, '', {
+  toString: function () {
+    return '('+this.x+', '+this.y+', '+this.z+')';
+  },
+  equals: function (p) {
+    return (this.x == p.x && this.y == p.y && this.z == p.z);
+  },
+  isZero: function () {
+    return (this.x == 0 && this.y == 0 && this.z == 0);
+  },
+  copy: function () {
+    return new Vec3(this.x, this.y, this.z);
+  },
+  norm2: function () {
+    return (this.x*this.x + this.y*this.y + this.z*this.z);
+  },
+  norm: function () {
+    return Math.sqrt(this.x*this.x + this.y*this.y + this.z*this.z);
+  },
+  sign: function () {
+    return new Vec3(sign(this.x), sign(this.y), sign(this.z));
+  },
+  add: function (v) {
+    return new Vec3(this.x+v.x, this.y+v.y, this.z+v.z);
+  },
+  sub: function (v) {
+    return new Vec3(this.x-v.x, this.y-v.y, this.z-v.z);
+  },
+  scale: function (v) {
+    return new Vec3(this.x*v, this.y*v, this.z*v);
+  },
+  distance: function (v) {
+    return this.sub(v).norm();
+  },
+  clamp: function (v) {
+    return new Vec3(
+      clamp(-v.x, this.x, +v.x),
+      clamp(-v.y, this.y, +v.y),
+      clamp(-v.z, this.z, +v.z));
+  },
+  move: function (dx, dy, dz) {
+    return new Vec3(this.x+dx, this.y+dy, this.z+dz);
+  },
+});
+
+
+// Rectangle
+function MakeRect(p, w, h)
+{
+  return new Rectangle(p.x, p.y, w, h);
+}
+function Rectangle(x, y, width, height)
+{
+  this.x = (x !== undefined)? x : 0;
+  this.y = (y !== undefined)? y : 0;
+  this.width = (width !== undefined)? width : 0;
+  this.height = (height !== undefined)? height : 0;
+}
+var Rect = Rectangle;
+define(Rectangle, Object, '', {
+  toString: function () {
+    return '('+this.x+', '+this.y+', '+this.width+', '+this.height+')';
+  },
+  equals: function (rect) {
+    return (this.x == rect.x && this.y == rect.y &&
+	    this.width == rect.width && this.height == rect.height);
+  },
+  right: function () {
+    return this.x+this.width;
+  },
+  bottom: function () {
+    return this.y+this.height;
+  },
+  centerx: function () {
+    return this.x+this.width/2;
+  },
+  centery: function () {
+    return this.y+this.height/2;
+  },
+  topleft: function () {
+    return new Vec2(this.x, this.y);
+  },
+  topright: function () {
+    return new Vec2(this.x+this.width, this.y);
+  },
+  bottomleft: function () {
+    return new Vec2(this.x, this.y+this.height);
+  },
+  bottomright: function () {
+    return new Vec2(this.x+this.width, this.y+this.height);
+  },
+  center: function () {
+    return new Vec2(this.x+this.width/2, this.y+this.height/2);
+  },
+  anchor: function (vx, vy) {
+    var x, y;
+    if (0 < vx) {
+      x = this.x;
+    } else if (vx < 0) {
+      x = this.x+this.width;
+    } else {
+      x = this.x+this.width/2;
+    }
+    if (0 < vy) {
+      y = this.y;
+    } else if (vy < 0) {
+      y = this.y+this.height;
+    } else {
+      y = this.y+this.height/2;
+    }
+    return new Vec2(x, y);
+  },
+  copy: function () {
+    return new Rectangle(this.x, this.y, this.width, this.height);
+  },
+  move: function (dx, dy) {
+    return new Rectangle(this.x+dx, this.y+dy, this.width, this.height);  
+  },
+  add: function (v) {
+    return new Rectangle(this.x+v.x, this.y+v.y, this.width, this.height);  
+  },
+  diff: function (rect) {
+    return new Vec2(this.x-rect.x, this.y-rect.y);
+  },
+  inflate: function (dw, dh) {
+    return new Rectangle(this.x-dw, this.y-dh, this.width+dw*2, this.height+dh*2);
+  },
+  expand: function (dw, dh, vx, vy) {
+    vx = (vx !== undefined)? vx : 0;
+    vy = (vy !== undefined)? vy : 0;
+    var x, y;
+    if (0 < vx) {
+      x = this.x;
+    } else if (vx < 0) {
+      x = this.x-dw;
+    } else {
+      x = this.x-dw/2;
+    }
+    if (0 < vy) {
+      y = this.y;
+    } else if (vy < 0) {
+      y = this.y-dh;
+    } else {
+      y = this.y-dh/2;
+    }
+    return new Rectangle(x, y, this.width+dw, this.height+dh);
+  },
+  contains: function (p) {
+    return (this.x <= p.x && this.y <= p.y &&
+	    p.x <= this.x+this.width && p.y <= this.y+this.height);
+  },
+  containsRect: function (rect) {
+    return (this.x <= rect.x &&
+	    this.y <= rect.y &&
+	    rect.x+rect.width <= this.x+this.width &&
+	    rect.y+rect.height <= this.y+this.height);
+  },
+  xdistance: function (rect) {
+    return Math.max(rect.x-(this.x+this.width),
+		    this.x-(rect.x+rect.width));
+  },
+  ydistance: function (rect) {
+    return Math.max(rect.y-(this.y+this.height),
+		    this.y-(rect.y+rect.height));
+  },
+  distance: function (rect) {
+    return new Vec2(this.xdistance(rect),
+		    this.ydistance(rect));
+  },
+  overlap: function (rect) {
+    return (this.xdistance(rect) < 0 &&
+	    this.ydistance(rect) < 0);
+  },
+  union: function (rect) {
+    var x0 = Math.min(this.x, rect.x);
+    var y0 = Math.min(this.y, rect.y);
+    var x1 = Math.max(this.x+this.width, rect.x+rect.width);
+    var y1 = Math.max(this.y+this.height, rect.y+rect.height);
+    return new Rectangle(x0, y0, x1-x0, y1-y0);
+  },
+  intersection: function (rect) {
+    var x0 = Math.max(this.x, rect.x);
+    var y0 = Math.max(this.y, rect.y);
+    var x1 = Math.min(this.x+this.width, rect.x+rect.width);
+    var y1 = Math.min(this.y+this.height, rect.y+rect.height);
+    return new Rectangle(x0, y0, x1-x0, y1-y0);
+  },
+  clamp: function (rect) {
+    var x = ((rect.width < this.width)? rect.centerx() :
+	     clamp(rect.x, this.x, rect.x+rect.width-this.width));
+    var y = ((rect.height < this.height)? rect.centery() :
+	     clamp(rect.y, this.y, rect.y+rect.height-this.height));
+    return new Rectangle(x, y, this.width, this.height);
+  },
+  rndpt: function () {
+    return new Vec2(this.x+rnd(this.width),
+		    this.y+rnd(this.height));
+  },
+  
+  contactVLine: function (v, x, y0, y1) {
+    var dx, dy;
+    var x0 = this.x;
+    var x1 = this.x+this.width;
+    if (x <= x0 && x0+v.x < x) {
+      dx = x-x0;
+    } else if (x1 <= x && x < x1+v.x) {
+      dx = x-x1;
+    } else {
+      return v;
+    }
+    dy = v.y*dx / v.x;
+    var y = this.y+dy;
+    if (y+this.height < y0 || y1 < y ||
+	(y+this.height == y0 && v.y <= 0) ||
+	(y1 == y && 0 <= v.y)) {
+      return v;
+    }
+    return new Vec2(dx, dy);
+  },
+  contactHLine: function (v, y, x0, x1) {
+    var dx, dy;
+    var y0 = this.y;
+    var y1 = this.y+this.height;
+    if (y <= y0 && y0+v.y < y) {
+      dy = y-y0;
+    } else if (y1 <= y && y < y1+v.y) {
+      dy = y-y1;
+    } else {
+      return v;
+    }
+    dx = v.x*dy / v.y;
+    var x = this.x+dx;
+    if (x+this.width < x0 || x1 < x ||
+	(x+this.width == x0 && v.x <= 0) ||
+	(x1 == x && 0 <= v.x)) {
+      return v;
+    }
+    return new Vec2(dx, dy);
+  },
+  contact: function (v, rect) {
+    assert(!this.overlap(rect), 'rect overlapped');
+    
+    if (0 < v.x) {
+      v = this.contactVLine(v, rect.x, rect.y, rect.y+rect.height);
+    } else if (v.x < 0) {
+      v = this.contactVLine(v, rect.x+rect.width, rect.y, rect.y+rect.height);
+    }
+
+    if (0 < v.y) {
+      v = this.contactHLine(v, rect.y, rect.x, rect.x+rect.width);
+    } else if (v.y < 0) {
+      v = this.contactHLine(v, rect.y+rect.height, rect.x, rect.x+rect.width);
+    }
+
+    return v;
+  },
+});
+
+
+// Box
+function Box(origin, size)
+{
+  this.origin = origin;
+  this.size = (size !== undefined)? size : new Vec3();
+}
+define(Box, Object, '', {
+  toString: function () {
+    return '('+this.origin+', '+this.size+')';
+  },
+  equals: function (box) {
+    return (this.origin.equals(box.origin) &&
+	    this.size.equals(box.size));
+  },
+  center: function () {
+    return new Vec3(this.origin.x+this.size.x/2,
+		    this.origin.y+this.size.y/2,
+		    this.origin.z+this.size.z/2);
+  },
+  copy: function () {
+    return new Box(this.origin.copy(), this.size.copy());
+  },
+  move: function (dx, dy, dz) {
+    return new Box(this.origin.move(dx, dy, dz), this.size);
+  },
+  add: function (v) {
+    return new Box(this.origin.add(v), this.size);
+  },
+  diff: function (box) {
+    return this.origin.sub(box.origin);
+  },
+  xdistance: function (box) {
+    return Math.max(box.origin.x-(this.origin.x+this.size.x),
+		    this.origin.x-(box.origin.x+box.size.x));
+  },
+  ydistance: function (box) {
+    return Math.max(box.origin.y-(this.origin.y+this.size.y),
+		    this.origin.y-(box.origin.y+box.size.y));
+  },
+  zdistance: function (box) {
+    return Math.max(box.origin.z-(this.origin.z+this.size.z),
+		    this.origin.z-(box.origin.z+box.size.z));
+  },
+  inflate: function (dx, dy, dz) {
+    return new Box(this.origin.move(-dx, -dy, -dz),
+		   this.size.move(dx*2, dy*2, dz*2));
+  },
+  contains: function (p) {
+    return (this.origin.x <= p.x && this.origin.y <= p.y && this.origin.z <= p.z &&
+	    p.x <= this.origin.x+this.size.x &&
+	    p.y <= this.origin.y+this.size.y &&
+	    p.z <= this.origin.z+this.size.z);
+  },
+  overlap: function (box) {
+    return (this.xdistance(box) < 0 &&
+	    this.ydistance(box) < 0 &&
+	    this.zdistance(box) < 0);
+  },
+  union: function (box) {
+    var x0 = Math.min(this.origin.x, box.origin.x);
+    var y0 = Math.min(this.origin.y, box.origin.y);
+    var z0 = Math.min(this.origin.z, box.origin.z);
+    var x1 = Math.max(this.origin.x+this.size.x, box.origin.x+box.size.x);
+    var y1 = Math.max(this.origin.y+this.size.y, box.origin.y+box.size.y);
+    var z1 = Math.max(this.origin.z+this.size.z, box.origin.z+box.size.z);
+    return new Box(new Vec3(x0, y0, z0),
+		   new Vec3(x1-x0, y1-y0, z1-z0));
+  },
+  intersection: function (box) {
+    var x0 = Math.max(this.origin.x, box.origin.x);
+    var y0 = Math.max(this.origin.y, box.origin.y);
+    var z0 = Math.max(this.origin.z, box.origin.z);
+    var x1 = Math.min(this.origin.x+this.size.x, box.origin.x+box.size.x);
+    var y1 = Math.min(this.origin.y+this.size.y, box.origin.y+box.size.y);
+    var z1 = Math.min(this.origin.z+this.size.z, box.origin.z+box.size.z);
+    return new Box(new Vec3(x0, y0, z0),
+		   new Vec3(x1-x0, y1-y0, z1-z0));
+  },
+  clamp: function (box) {
+    var x = ((box.size.x < this.size.x)? (box.origin.x+box.size.x/2) :
+	     clamp(box.origin.x, this.origin.x, box.origin.x+box.size.x-this.size.x));
+    var y = ((box.size.y < this.size.y)? (box.origin.y+box.size.y/2) :
+	     clamp(box.origin.y, this.origin.y, box.origin.y+box.size.y-this.size.y));
+    var z = ((box.size.z < this.size.z)? (box.origin.z+box.size.z/2) :
+	     clamp(box.origin.z, this.origin.z, box.origin.z+box.size.z-this.size.z));
+    return new Box(new Vec3(x, y, z), this.size);
+  },
+  rndpt: function () {
+    return new Vec3(this.origin.x+rnd(this.size.x),
+		    this.origin.y+rnd(this.size.y),
+		    this.origin.z+rnd(this.size.z));
+  },
+
+  contactYZPlane: function (v, x, rect) {
+    var dx, dy, dz;
+    var x0 = this.origin.x;
+    var x1 = this.origin.x+this.size.x;
+    if (x <= x0 && x0+v.x < x) {
+      dx = x-x0;
+    } else if (x1 <= x && x < x1+v.x) {
+      dx = x-x1;
+    } else {
+      return v;
+    }
+    dy = v.y*dx / v.x;
+    dz = v.z*dx / v.x;
+    if (rect !== null) {
+      var y = this.origin.y+dy;
+      var z = this.origin.z+dz;
+      if (y+this.size.y < rect.x || rect.x+rect.width < y ||
+	  z+this.size.z < rect.y || rect.y+rect.height < z ||
+	  (y+this.size.y == rect.x && v.y <= 0) ||
+	  (rect.x+rect.width == y && 0 <= v.y) ||
+	  (z+this.size.z == rect.y && v.z <= 0) ||
+	  (rect.y+rect.height == z && 0 <= v.z)) {
+	return v;
+      }
+    }
+    return new Vec3(dx, dy, dz);
+  },
+  contactZXPlane: function (v, y, rect) {
+    var dx, dy, dz;
+    var y0 = this.origin.y;
+    var y1 = this.origin.y+this.size.y;
+    if (y <= y0 && y0+v.y < y) {
+      dy = y-y0;
+    } else if (y1 <= y && y < y1+v.y) {
+      dy = y-y1;
+    } else {
+      return v;
+    }
+    dz = v.z*dy / v.y;
+    dx = v.x*dy / v.y;
+    if (rect !== null) {
+      var z = this.origin.z+dz;
+      var x = this.origin.x+dx;
+      if (z+this.size.z < rect.x || rect.x+rect.width < z ||
+	  x+this.size.x < rect.y || rect.y+rect.height < x ||
+	  (z+this.size.z == rect.x && v.z <= 0) ||
+	  (rect.x+rect.width == z && 0 <= v.z) ||
+	  (x+this.size.x == rect.y && v.x <= 0) ||
+	  (rect.y+rect.height == x && 0 <= v.x)) {
+	return v;
+      }
+    }
+    return new Vec3(dx, dy, dz);  
+  },
+  contactXYPlane: function (v, z, rect) {
+    var dx, dy, dz;
+    var z0 = this.origin.z;
+    var z1 = this.origin.z+this.size.z;
+    if (z <= z0 && z0+v.z < z) {
+      dz = z-z0;
+    } else if (z1 <= z && z < z1+v.z) {
+      dz = z-z1;
+    } else {
+      return v;
+    }
+    dx = v.x*dz / v.z;
+    dy = v.y*dz / v.z;
+    if (rect !== null) {
+      var x = this.origin.x+dx;
+      var y = this.origin.y+dy;
+      if (x+this.size.x < rect.x || rect.x+rect.width < x ||
+	  y+this.size.y < rect.y || rect.y+rect.height < y ||
+	  (x+this.size.x == rect.x && v.x <= 0) ||
+	  (rect.x+rect.width == x && 0 <= v.x) ||
+	  (y+this.size.y == rect.y && v.y <= 0) ||
+	  (rect.y+rect.height == y && 0 <= v.y)) {
+	return v;
+      }
+    }
+    return new Vec3(dx, dy, dz);  
+  },
+  contact: function (v, box) {
+    assert(!this.overlap(box), 'box overlapped');
+    
+    if (0 < v.x) {
+      v = this.contactYZPlane(v, box.origin.x, 
+			      new Rectangle(box.origin.y, box.origin.z,
+					    box.size.y, box.size.z));
+    } else if (v.x < 0) {
+      v = this.contactYZPlane(v, box.origin.x+box.size.x, 
+			      new Rectangle(box.origin.y, box.origin.z,
+					    box.size.y, box.size.z));
+    }
+
+    if (0 < v.y) {
+      v = this.contactZXPlane(v, box.origin.y, 
+			      new Rectangle(box.origin.z, box.origin.x,
+					    box.size.z, box.size.x));
+    } else if (v.y < 0) {
+      v = this.contactZXPlane(v, box.origin.y+box.size.y, 
+			      new Rectangle(box.origin.z, box.origin.x,
+					    box.size.z, box.size.x));
+    }
+    
+    if (0 < v.z) {
+      v = this.contactXYPlane(v, box.origin.z, 
+			      new Rectangle(box.origin.x, box.origin.y,
+					    box.size.x, box.size.y));
+    } else if (v.z < 0) {
+      v = this.contactXYPlane(v, box.origin.z+box.size.z, 
+			      new Rectangle(box.origin.x, box.origin.y,
+					    box.size.x, box.size.y));
+    }
+    
+    return v;
+  },
+});
