3be54975a297843f66854c13e108a67070180b2e
Yusuke Shinyama <yusuke@shinyama.jp>
2016-11-27 15:53:54 +0900

    Added: drunksheriff.

---
+++ drunksheriff/base/utils.ts
@@ -0,0 +1,409 @@
+/** 
+ * Utility functions.
+ */
+
+/** Alias of window.console.log() */
+const log = window.console.log.bind(window.console);
+
+/** Raises an exception if the condition is not met. */
+function assert(x: boolean, msg="assertion error")
+{
+    if (!x) {
+	throw new Error(msg);
+    }
+}
+
+/** Creates a mixin class. */
+function applyMixins(derivedCtor: any, baseCtors: any[]) {
+    baseCtors.forEach(baseCtor => {
+	Object.getOwnPropertyNames(baseCtor.prototype).forEach(name => {
+	    derivedCtor.prototype[name] = baseCtor.prototype[name];
+	});
+    });
+}
+
+// fmod(x, y):
+function fmod(x: number, y: number)
+{
+    const v = x % y;
+    return (0 <= v)? v : v+y;
+}
+
+// int(x):
+const int = Math.floor;
+
+// upperbound(x, y):
+const upperbound = Math.min;
+
+// lowerbound(x, y):
+const lowerbound = Math.max;
+
+/** Limits the value so that v0 <= v <= v1. */
+function clamp(v0: number, v: number, v1: number)
+{
+    return Math.min(Math.max(v, v0), v1);
+}
+
+/** Returns -1, 0, or +1 depending on the sign. */
+function sign(v: number)
+{
+    if (v < 0) {
+	return -1;
+    } else if (0 < v) {
+	return +1;
+    } else {
+	return 0;
+    }
+}
+
+/** Returns the phase for t with the given interval. */
+function phase(t: number, interval: number, n=2)
+{
+    if (interval === 0) return 0;
+    return int(n*t/interval) % n;
+}
+
+/** Generates a random number in [0,a) or [a,b). */
+function frnd(a: number, b=0)
+{
+    if (b < a) {
+	const c = a;
+	a = b;
+	b = c;
+    }
+    return a+(Math.random()*(b-a));
+}
+
+/** Generates an integer random number in [0,a) or [a,b). */
+function rnd(a: number, b=0)
+{
+    return int(frnd(a, b));
+}
+
+/** Returns a pretty printed string. 
+ * @param v Number to format.
+ * @param n Number of digits to fill.
+ * @param c Filler character.
+ */
+function format(v: number, n=3, c=' ')
+{
+    let s = '';
+    while (s.length < n) {
+	s = (v % 10)+s;
+	v = int(v/10);
+	if (v <= 0) break;
+    }
+    while (s.length < n) {
+	s = c+s;
+    }
+    return s;
+}
+
+/** Picks a random element. */
+function choice<T>(a: T[])
+{
+    return a[rnd(a.length)];
+}
+
+/** Removes an element. */
+function removeElement<T>(a: T[], obj: T)
+{
+    const i = a.indexOf(obj);
+    if (0 <= i) {
+	a.splice(i, 1);
+    }
+    return a;
+}
+
+/** Creates an array from a string.
+ * @param s Source string.
+ * @param f Conversion function.
+ */
+function str2array(s: string, f: (c:string)=>number=parseInt)
+{
+    const a = new Int32Array(s.length);
+    for (let i = 0; i < s.length; i++) {
+	a[i] = f(s[i]);
+    }
+    return a;
+}
+
+/** Removes all child DOM Nodes with the given name. 
+ * @param node Parent DOM Node.
+ * @param name Name of child Nodes to be removed.
+ */
+function removeChildren(node: Node, name: string)
+{
+    name = name.toLowerCase();
+    // Iterate backwards to simplify array removal. (thanks to @the31)
+    for (let i = node.childNodes.length-1; 0 <= i; i--) {
+	const c = node.childNodes[i];
+	if (c.nodeName.toLowerCase() === name) {
+	    node.removeChild(c);
+	}
+    }
+}
+
+/** Creates a canvas with the given size.
+ * @param width Width.
+ * @param height Height.
+ */
+function createCanvas(width: number, height: number): HTMLCanvasElement
+{
+    const canvas = document.createElement('canvas');
+    canvas.width = width;
+    canvas.height = height;
+    return canvas;
+}
+
+/** Creates a pixellated canvas 2D context.
+ * @param canvas Target Canvas object.
+ */
+function getEdgeyContext(canvas: HTMLCanvasElement): CanvasRenderingContext2D
+{
+    const ctx = canvas.getContext('2d');
+    (ctx as any).imageSmoothingEnabled = false;
+    (ctx as any).webkitImageSmoothingEnabled = false;
+    (ctx as any).mozImageSmoothingEnabled = false;
+    (ctx as any).msImageSmoothingEnabled = false;
+    return ctx;
+}
+
+/** Creates a 2D array from an image. */
+function image2array(img: HTMLImageElement)
+{
+    interface ColorMap {
+	[index:number]: number;
+    }
+    const header = 1;
+    const width = img.width;
+    const height = img.height;
+    const canvas = createCanvas(width, height);
+    const ctx = getEdgeyContext(canvas);
+    ctx.drawImage(img, 0, 0);
+    const data = ctx.getImageData(0, 0, width, height).data;
+    let i = 0;
+    let c2v:ColorMap = {};
+    for (let y = 0; y < header; y++) {
+	for (let x = 0; x < width; x++, i+=4) {
+	    const c = ((data[i] << 16) | (data[i+1] << 8) | data[i+2]); // RGBA
+	    if (!c2v.hasOwnProperty(c.toString())) {
+		c2v[c] = y*width + x;
+	    }
+	}
+    }
+    const map = new Array(height-header);
+    for (let y = 0; y < height-header; y++) {
+	const a = new Int32Array(width);
+	for (let x = 0; x < width; x++, i+=4) {
+	    const c = ((data[i] << 16) | (data[i+1] << 8) | data[i+2]); // RGBA
+	    a[x] = c2v[c];
+	}
+	map[y] = a;
+    }
+    return map;
+}
+
+/** Draw a scaled image. 
+ *  When the destination width/height is negative, 
+ *  the image is flipped.
+ */
+function drawImageScaled(
+    ctx: CanvasRenderingContext2D,
+    src: HTMLImageElement,
+    sx: number, sy: number, sw: number, sh: number,
+    dx: number, dy: number, dw: number, dh: number)
+{
+    ctx.save();
+    ctx.translate(dx+((0 < dw)? 0 : -dw),
+		  dy+((0 < dh)? 0 : -dh));
+    ctx.scale((0 < dw)? 1 : -1,
+	      (0 < dh)? 1 : -1);
+    ctx.drawImage(src, sx, sy, sw, sh, 0, 0,
+		  Math.abs(dw), Math.abs(dh));
+    ctx.restore();
+}
+
+/** Play a sound resource. */
+function playSound(sound: HTMLAudioElement, start=0)
+{
+    sound.currentTime = start;
+    sound.play();
+}
+
+/** Key Symbol */
+enum KeySym {
+    Unknown = 0,
+    Left,
+    Right,
+    Up,
+    Down,
+    Action,
+    Cancel,
+}
+
+/** Returns the key symbol for the key.
+ * cf. https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/keyCode
+ */
+function getKeySym(keyCode: number): KeySym
+{
+    switch (keyCode) {
+    case 37:			// LEFT
+    case 65:			// A
+    case 72:			// H
+	return KeySym.Left;
+    case 39:			// RIGHT
+    case 68:			// D
+    case 76:			// L
+	return KeySym.Right;
+    case 38:			// UP
+    case 87:			// W
+    case 75:			// K
+	return KeySym.Up;
+    case 40:			// DOWN
+    case 83:			// S
+    case 74:			// J
+	return KeySym.Down;
+    case 13:			// ENTER
+    case 16:			// SHIFT
+    case 32:			// SPACE
+    case 90:			// Z
+    case 89:			// Y (QWERTZ)
+	return KeySym.Action;
+    case 8:			// BACKSPACE
+    case 27:			// ESCAPE
+    case 88:			// X
+	return KeySym.Cancel;
+    default:
+	return KeySym.Unknown;
+    }
+}
+
+
+interface Action {
+    (...params:any[]): void;
+}
+
+/** Subscribable event object.
+ *  A Signal object can have multiple receivers.
+ */
+class Signal {
+
+    /** Sender object. */
+    sender: any;
+    /** List of receivers. */
+    receivers: Action[] = [];
+
+    /** Creates a new Signal.
+     * @param sender Sender object.
+     */
+    constructor(sender: any) {
+	this.sender = sender;
+    }
+	
+    toString() {
+	return ('<Signal('+this.sender+') '+this.receivers+'>');
+    }
+  
+    /** Adds a receiver function for the signal.
+     * @param recv Receiver function to add.
+     */
+    subscribe(recv: Action) {
+	this.receivers.push(recv);
+    }
+  
+    /** Removes a receiver function for the signal.
+     * @param recv Receiver function to remove.
+     */
+    unsubscribe(recv: Action) {
+	removeElement(this.receivers, recv);
+    }
+
+    /** Notifies all the receivers with the given arguments. */
+    fire(...params: any[]) {
+	for (let receiver of this.receivers) {
+	    const args = Array.prototype.slice.call(arguments);
+	    args.unshift(this.sender);
+	    receiver.apply(null, args);
+	}
+    }
+}
+
+
+/** Convenience object for generating/mixing RGB values.
+ */
+class Color {
+
+    /** Red. */
+    r: number;
+    /** Green. */
+    g: number;
+    /** Blue. */
+    b: number;
+    /** Alpha. */
+    a: number;
+
+    /** Generates a Color value. 
+     * @param h Hue.
+     * @param v Brightness.
+     */
+    static generate(h: number, v: number=1.0) {
+	h *= 2*Math.PI;
+	v *= 0.5;
+	return new Color(
+	    (Math.sin(h)+1)*v,
+	    (Math.sin(h+2*Math.PI/3)+1)*v,
+	    (Math.sin(h+4*Math.PI/3)+1)*v,
+	    1.0);
+    }
+
+    /** Creates a new Color.
+     * @param r Red.
+     * @param g Green.
+     * @param b Blue.
+     * @param a Alpha.
+     */
+    constructor(r: number, g: number, b: number, a=1.0) {
+	this.r = r;
+	this.g = g;
+	this.b = b;
+	this.a = a;
+    }
+    
+    toString() {
+	if (0 <= this.a) {
+	    return ('rgba('+
+		    int(255*clamp(0,this.r,1))+','+
+		    int(255*clamp(0,this.g,1))+','+
+		    int(255*clamp(0,this.b,1))+','+
+		    clamp(0,this.a,1)+')');
+	} else {
+	    return ('rgb('+
+		    int(255*clamp(0,this.r,1))+','+
+		    int(255*clamp(0,this.g,1))+','+
+		    int(255*clamp(0,this.b,1))+')');
+	}
+    }
+
+    /** Multiplies the brightness. */
+    multiply(t: number) {
+	return new Color(this.r*t, this.g*t, this.b*t, this.a);
+    }
+
+    /** Changes the alpha value. */
+    setAlpha(a: number) {
+	return new Color(this.r, this.g, this.b, a);
+    }
+
+    /** Blends with another Color. 
+     * @param color Color to bland.
+     * @param t Blending ratio.
+     */
+    blend(color: Color, t: number) {
+	return new Color(
+	    this.r*(1-t) + color.r*t,
+	    this.g*(1-t) + color.g*t,
+	    this.b*(1-t) + color.b*t,
+	    this.a*(1-t) + color.a*t);
+    }
+}
---
+++ drunksheriff/src/game.ts
@@ -0,0 +1,295 @@
+/// <reference path="../base/utils.ts" />
+/// <reference path="../base/geom.ts" />
+/// <reference path="../base/entity.ts" />
+/// <reference path="../base/text.ts" />
+/// <reference path="../base/scene.ts" />
+/// <reference path="../base/app.ts" />
+
+///  game.ts
+///
+
+
+//  Initialize the resources.
+let FONT: Font;
+let SPRITES:ImageSpriteSheet;
+addInitHook(() => {
+    FONT = new ShadowFont(IMAGES['font'], 'white');
+    SPRITES = new ImageSpriteSheet(
+	IMAGES['sprites'], new Vec2(8,8), new Vec2(4,4));
+});
+const BULLET = new FillImageSource('#222', new Rect(-1,-1,2,2));
+const S_LAND = 0;
+const S_CACTUS = 1;
+const S_PLAYER = 2;
+const S_SNAKE = 3;
+const S_BANDIDO = 4;
+const S_PRESENT = 5;
+
+
+//  Cactus
+//
+class Cactus extends Entity {
+    constructor(pos: Vec2) {
+	super(pos);
+	this.sprite.imgsrc = SPRITES.get(S_CACTUS);
+	this.collider = this.sprite.getBounds(new Vec2()).inflate(-2,-2);
+    }
+}
+
+
+//  Bullet
+//
+class Bullet extends Projectile {
+    constructor(pos: Vec2, game: Game, movement: Vec2) {
+	super(pos);
+	this.sprite.imgsrc = BULLET;
+	this.collider = new Rect(-2,-2,4,4);
+	this.frame = game.screen;
+	this.movement = movement;
+    }
+}
+
+
+//  Present
+//
+class Present extends Projectile {
+    constructor(pos: Vec2, game: Game, movement: Vec2) {
+	super(pos);
+	this.sprite.imgsrc = SPRITES.get(S_PRESENT);
+	this.collider = new Rect(-2,-2,4,4);
+	this.frame = game.screen;
+	this.movement = movement;
+    }
+}
+
+
+//  WorldEntity
+//
+class WorldEntity extends Entity {
+    game: Game;
+
+    constructor(game: Game, pos: Vec2) {
+	super(pos);
+	this.game = game;
+    }
+    
+    getObstaclesFor(range: Rect, v: Vec2, context: string): Collider[] {
+	let ents = this.game.layer.findEntities((e:Entity) => {
+	    return (e instanceof Cactus);
+	});
+	return ents.map((e:Entity) => { return e.getCollider() as Rect; });
+    }
+
+    getFencesFor(range: Rect, v: Vec2, context: string): Rect[] {
+	return [this.game.screen];
+    }
+}
+
+
+//  Player
+//
+class Player extends WorldEntity {
+
+    vx = 1;
+    usermove: Vec2;
+
+    constructor(game: Game, pos: Vec2) {
+	super(game, pos);
+	this.sprite.imgsrc = SPRITES.get(S_PLAYER);
+	this.collider = this.sprite.getBounds(new Vec2()).inflate(-2,-2);
+	this.usermove = new Vec2();
+    }
+
+    update() {
+	super.update();
+	this.moveIfPossible(this.usermove);
+    }
+
+    fire() {
+	if (this.running) {
+	    let v = new Vec2(this.vx*8, (rnd(2)*2-1)*rnd(2,4));
+	    this.game.add(new Present(this.pos.move(this.vx*8,0), this.game, v));
+	    playSound(SOUNDS['gun']);
+	}
+    }
+    
+    setMove(v: Vec2) {
+	if (rnd(3) == 0) {
+	    v = v.rot90(rnd(2)*2-1);
+	}
+	this.usermove = v.scale(2);
+	if (v.x != 0) {
+	    this.vx = v.x;
+	    this.sprite.scale.x = this.vx;
+	}
+    }
+
+    collidedWith(e: Entity) {
+	if (e instanceof Bullet) {
+	    playSound(SOUNDS['dead']);
+	    this.stop();
+	}
+    }
+}
+
+
+//  Bandido
+//
+class Bandido extends WorldEntity {
+
+    vx = 1;
+    nextTurn = 0;
+    nextFire = 0;
+    movement = new Vec2(+1,0);
+    
+    constructor(game: Game, pos: Vec2) {
+	super(game, pos);
+	this.sprite.imgsrc = SPRITES.get(S_BANDIDO);
+	this.collider = this.sprite.getBounds(new Vec2()).inflate(-2,-2);
+    }
+
+    fire() {
+	let v = new Vec2(this.vx*8, 0);
+	this.game.add(new Bullet(this.pos.move(this.vx*8,0), this.game, v));
+    }
+    
+    update() {
+	super.update();
+	if (this.nextTurn == 0) {
+	    this.movement = this.movement.rot90(rnd(4));
+	    this.sprite.scale.x = this.movement.x;
+	    this.nextTurn = rnd(1, 10);
+	}
+	this.nextTurn--;
+	if (this.nextFire == 0) {
+	    this.fire();
+	    this.nextFire = rnd(20, 40);
+	}
+	this.nextFire--;
+	if (this.movement.x != 0) {
+	    this.vx = this.movement.x;
+	}
+	this.moveIfPossible(this.movement);
+    }
+
+    collidedWith(e: Entity) {
+	if (e instanceof Present) {
+	    this.game.addScore();
+	    this.stop();
+	} else if (e instanceof Bullet) {
+	    this.stop();
+	}
+    }
+}
+
+
+//  Snake
+//
+class Snake extends WorldEntity {
+
+    nextTurn = 0;
+    movement = new Vec2(+1,0);
+    
+    constructor(game: Game, pos: Vec2) {
+	super(game, pos);
+	this.sprite.imgsrc = SPRITES.get(S_SNAKE);
+	this.collider = this.sprite.getBounds(new Vec2()).inflate(-2,-2);
+    }
+
+    update() {
+	super.update();
+	if (this.nextTurn == 0) {
+	    this.movement = this.movement.rot90(+1);
+	    this.sprite.scale.x = this.movement.x;
+	    this.nextTurn = rnd(1, 10);
+	}
+	this.nextTurn--;
+	this.moveIfPossible(this.movement);
+    }
+
+    collidedWith(e: Entity) {
+	if (e instanceof Present) {
+	    this.game.addScore();
+	    this.stop();
+	} else if (e instanceof Bullet) {
+	    this.stop();
+	}
+    }
+}
+
+
+//  Game
+// 
+class Game extends GameScene {
+
+    player: Player;
+    spawnArea: Rect;
+    snakeSpawn = 0;
+    bandidoSpawn = 0;
+    scoreBox: TextBox;
+    score: number;
+    
+    init() {
+	super.init();
+	this.scoreBox = new TextBox(this.screen.inflate(-2,-2), FONT);
+	this.player = new Player(this, this.screen.center());
+	this.player.stopped.subscribe(() => { this.gameOver(); });
+	this.add(this.player);
+	
+	this.spawnArea = this.screen.inflate(-8, -8);
+	for (let i = 0; i < 10; i++) {
+	    this.add(new Cactus(this.screen.rndpt()));
+	}
+	this.score = 0;
+	this.updateScore();
+    }
+
+    update() {
+	super.update();
+	if (this.snakeSpawn == 0) {
+	    this.add(new Snake(this, this.spawnArea.rndptEdge()));
+	    this.snakeSpawn = rnd(20,40);
+	}
+	this.snakeSpawn--;
+	if (this.bandidoSpawn == 0) {
+	    this.add(new Bandido(this, this.spawnArea.rndptEdge()));
+	    this.bandidoSpawn = rnd(20,40);
+	}
+	this.bandidoSpawn--;
+    }
+
+    setDir(v: Vec2) {
+	this.player.setMove(v);
+    }
+
+    setAction(action: boolean) {
+	if (action) {
+	    this.player.fire();
+	}
+    }
+
+    render(ctx: CanvasRenderingContext2D, bx: number, by: number) {
+	ctx.fillStyle = 'rgb(255,255,128)';
+	ctx.fillRect(bx, by, this.screen.width, this.screen.height);
+	super.render(ctx, bx, by);
+	this.scoreBox.render(ctx, bx, by);
+    }
+
+    gameOver() {
+	this.add(new BannerBox(this.screen, FONT, ['GAME OVER!']));
+	this.add(new DelayTask(2, ()=>{
+	    this.init();
+	}));
+    }
+
+    addScore() {
+	this.score++;
+	this.updateScore();
+	playSound(SOUNDS['got']);
+    }
+
+    updateScore() {
+	this.scoreBox.clear();
+	this.scoreBox.putText(['SCORE: '+this.score]);
+    }
+}
