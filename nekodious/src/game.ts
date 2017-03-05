2d1690dc215ef65b19c502de8d5c7beea772b412
Yusuke Shinyama <yusuke@shinyama.jp>
2017-03-05 13:29:52 +0900

    Added: nekodious: assets

--- nekodious/src/game.ts
+++ nekodious/src/game.ts
@@ -14,12 +14,15 @@ const APIKEY = 'WonderfulCheeryHedgehog';
 let FONT: Font;
 let SPRITES:ImageSpriteSheet;
 let NEKO:ImageSource;
+let EXPLOSION:ImageSource;
 addInitHook(() => {
     FONT = new ShadowFont(IMAGES['font'], 'white');
     SPRITES = new ImageSpriteSheet(
 	IMAGES['sprites'], new Vec2(16,16), new Vec2(8,8));
     NEKO = new HTMLImageSource(
 	IMAGES['sprites'], new Rect(0,16,48,32), new Rect(-32,-16,48,32));    
+    EXPLOSION = new HTMLImageSource(
+	IMAGES['sprites'], new Rect(48,16,32,32), new Rect(-16,-16,32,32));    
 });
 
 
@@ -40,8 +43,28 @@ class Bullet extends Projectile {
 class Explosion extends Entity {
     constructor(pos: Vec2) {
 	super(pos);
-	this.sprite.imgsrc = SPRITES.get(4);
+	this.sprite.imgsrc = EXPLOSION;
 	this.lifetime = 0.2;
+    }
+}
+
+
+//  Thingy
+//
+class Thingy extends Entity {
+    
+    game: Game;
+    
+    constructor(game: Game, pos: Vec2) {
+	super(pos);
+	this.game = game;
+	this.sprite.imgsrc = SPRITES.get(4);
+	this.collider = this.sprite.getBounds(new Vec2()).inflate(-8,-4);
+    }
+
+    update() {
+	super.update();
+	this.movePos(new Vec2(this.game.speed, 0));
     }
 }
 
@@ -59,7 +82,7 @@ class Player extends Entity {
 	super(pos);
 	this.game = game;
 	this.sprite.imgsrc = NEKO;
-	this.collider = this.sprite.getBounds(new Vec2());
+	this.collider = this.sprite.getBounds(new Vec2()).inflate(-12,-8);
     }
 
     update() {
@@ -96,13 +119,15 @@ class Player extends Entity {
     }
     
     getFencesFor(range: Rect, v: Vec2, context: string): Rect[] {
-	// Restrict its position within the screen.
 	return [this.game.screen];
     }
 
     collidedWith(entity: Entity) {
 	if (entity instanceof EnemyBase) {
 	    this.die();
+	} else if (entity instanceof Thingy) {
+	    entity.stop();
+	    this.powerup();
 	}
     }
 
@@ -110,6 +135,10 @@ class Player extends Entity {
 	playSound(SOUNDS['explosion']);
 	this.stop();
 	this.chain(new Explosion(this.pos));
+    }
+
+    powerup() {
+	playSound(SOUNDS['powerup']);
     }
 }
 
@@ -120,19 +149,25 @@ class Player extends Entity {
 //
 class EnemyBase extends Projectile {
 
-    killed: Signal;
-
+    game: Game;
+    
     constructor(game: Game, pos: Vec2) {
 	super(pos);
+	this.game = game;
 	this.frame = game.screen;
-	this.killed = new Signal(this);
     }
     
+    update() {
+	super.update();
+	this.movePos(new Vec2(this.game.speed, 0));
+    }
+
     collidedWith(entity: Entity) {
 	if (entity instanceof Bullet) {
+	    playSound(SOUNDS['explosion']);
 	    this.stop();
-	    this.killed.fire();
 	    this.chain(new Explosion(this.pos));
+	    this.game.addScore();
 	}
     }
 }
@@ -144,7 +179,7 @@ class Enemy1 extends EnemyBase {
 
     constructor(game: Game, pos: Vec2) {
 	super(game, pos);
-	this.sprite.imgsrc = SPRITES.get(1);
+	this.sprite.imgsrc = SPRITES.get(2);
 	this.collider = this.sprite.getBounds(new Vec2());
 	this.movement = new Vec2(-rnd(1,8), rnd(3)-1);
     }
@@ -157,7 +192,7 @@ class Enemy2 extends EnemyBase {
 
     constructor(game: Game, pos: Vec2) {
 	super(game, pos);
-	this.sprite.imgsrc = SPRITES.get(2);
+	this.sprite.imgsrc = SPRITES.get(3);
 	this.collider = this.sprite.getBounds(new Vec2());
 	this.movement = new Vec2(-rnd(1,4), 0);
     }
@@ -195,18 +230,19 @@ class Terrain extends TileMap {
 	    this.shift(-dw, 0);
 	    // Generate new tiles.
 	    for (let x = this.width-dw; x < this.width; x++) {
+		let t = rnd(2)+1;
 		for (let y = 0; y < this.height; y++) {
 		    let c = 0;
 		    if (0 < this.cy) {
-			c = (y < this.cy)? 1 : 0;
+			c = (y < this.cy)? t : 0;
 		    } else if (this.cy < 0) {
-			c = (this.height+this.cy <= y)? 1 : 0;
+			c = (this.height+this.cy <= y)? t : 0;
 		    }
 		    this.set(x, y, c);
 		}
 	    }
 	    this.offset = dx;
-	    this.cy = clamp(-this.height+1, this.cy+rnd(3)-1, this.height-1);
+	    this.cy = clamp(-this.height+2, this.cy+rnd(3)-1, this.height-2);
 	}
     }
 
@@ -228,6 +264,7 @@ class Game extends GameScene {
     scoreBox: TextBox;
     terrain: Terrain;
     stars: StarSprite;
+    speed: number;
 
     init() {
 	super.init();
@@ -237,6 +274,7 @@ class Game extends GameScene {
 	this.add(this.player);
 	this.terrain = new Terrain(22, 15);
 	this.stars = new StarSprite(this.screen, 100);
+	this.speed = -4;
 	this.nextenemy = 0;
 	this.score = 0;
 	this.updateScore();
@@ -245,27 +283,15 @@ class Game extends GameScene {
 
     update() {
 	super.update();
-	this.stars.move(new Vec2(-4, 0));
+	this.stars.move(new Vec2(this.speed, 0));
 	// Spawn an enemy at a random interval.
 	if (this.nextenemy == 0) {
-	    let pos = new Vec2(this.screen.width, rnd(this.screen.height));
-	    let enemy:EnemyBase;
-	    if (rnd(2) == 0) {
-		enemy = new Enemy1(this, pos);
-	    } else {
-		enemy = new Enemy2(this, pos);
-	    }
-	    // Increase the score when it's killed.
-	    enemy.killed.subscribe(() => {
-		playSound(SOUNDS['explosion']);
-		this.score++;
-		this.updateScore();
-	    });
-	    this.add(enemy);
+	    this.spawnEnemy();
 	    this.nextenemy = 10+rnd(20);
+	} else {
+	    this.nextenemy--;
 	}
 	this.terrain.proceed(4);
-	this.nextenemy--;
     }
 
     onButtonPressed(keysym: KeySym) {
@@ -278,14 +304,36 @@ class Game extends GameScene {
 	this.player.setMove(v);
     }
 
+    spawnEnemy() {
+	let pos = new Vec2(this.screen.width, rnd(this.screen.height));
+	let enemy:EnemyBase;
+	switch (rnd(2)) {
+	case 0:
+	    enemy = new Enemy1(this, pos);
+	    break;
+	case 1:
+	    enemy = new Enemy2(this, pos);
+	    break;
+	}
+	this.add(enemy);
+	if (rnd(2) == 0) {
+	    let pos = new Vec2(this.screen.width, rnd(this.screen.height));
+	    this.add(new Thingy(this, pos));
+	}
+    }
+
+    addScore() {
+	this.score++;
+	this.updateScore();
+    }
+
     updateScore() {
-	// Update the text in the score box.
 	this.scoreBox.clear();
 	this.scoreBox.putText(['SCORE:'+format(this.score)]);
     }
 
     render(ctx: CanvasRenderingContext2D, bx: number, by: number) {
-	ctx.fillStyle = 'rgb(0,0,200)';
+	ctx.fillStyle = 'rgb(50,0,100)';
 	ctx.fillRect(bx, by, this.screen.width, this.screen.height);
 	super.render(ctx, bx, by);
 	this.stars.render(ctx, bx, by);
