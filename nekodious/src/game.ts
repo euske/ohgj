ec4788b04c32a2a47949adb6f9391468c032f992
Yusuke Shinyama <yusuke@shinyama.jp>
2017-03-05 20:51:33 +0900

    Game is done, but...

--- nekodious/src/game.ts
+++ nekodious/src/game.ts
@@ -2,6 +2,8 @@
 /// <reference path="../base/geom.ts" />
 /// <reference path="../base/entity.ts" />
 /// <reference path="../base/text.ts" />
+/// <reference path="../base/tilemap.ts" />
+/// <reference path="../base/animation.ts" />
 /// <reference path="../base/scene.ts" />
 /// <reference path="../base/app.ts" />
 
@@ -10,6 +12,7 @@
 
 
 //  Initialize the resources.
+const APIURL = 'https://dollarone.games/elympics/submitHighscore';
 const APIKEY = 'WonderfulCheeryHedgehog';
 let FONT: Font;
 let SPRITES:ImageSpriteSheet;
@@ -59,7 +62,7 @@ class Thingy extends Entity {
 	super(pos);
 	this.game = game;
 	this.sprite.imgsrc = SPRITES.get(4);
-	this.collider = this.sprite.getBounds(new Vec2()).inflate(-8,-4);
+	this.collider = this.sprite.getBounds(new Vec2()).inflate(-4,-2);
     }
 
     update() {
@@ -76,13 +79,16 @@ class Player extends Entity {
     game: Game;
     usermove: Vec2 = new Vec2();
     firing: boolean = false;
-    nextfire: number = 0;	// Firing counter
+    nextfire: number = 0;
+
+    movespeed = 2;
+    firespeed = 8;
 
     constructor(game: Game, pos: Vec2) {
 	super(pos);
 	this.game = game;
 	this.sprite.imgsrc = NEKO;
-	this.collider = this.sprite.getBounds(new Vec2()).inflate(-12,-8);
+	this.collider = this.sprite.getBounds(new Vec2()).inflate(-8,-4);
     }
 
     update() {
@@ -90,13 +96,12 @@ class Player extends Entity {
 	this.moveIfPossible(this.usermove);
 	if (this.firing) {
 	    if (this.nextfire == 0) {
-		// Shoot a bullet at a certain interval.
 		var bullet = new Bullet(this.pos);
 		bullet.movement = new Vec2(8, 0);
 		bullet.frame = this.game.screen;
 		this.game.add(bullet);
 		playSound(SOUNDS['shoot']);
-		this.nextfire = 4;
+		this.nextfire = this.firespeed;
 	    }
 	    this.nextfire--;
 	}
@@ -109,13 +114,12 @@ class Player extends Entity {
     setFire(firing: boolean) {
 	this.firing = firing;
 	if (!this.firing) {
-	    // Reset the counter when start shooting.
 	    this.nextfire = 0;
 	}
     }
     
     setMove(v: Vec2) {
-	this.usermove = v.scale(4);
+	this.usermove = v.scale(this.movespeed);
     }
     
     getFencesFor(range: Rect, v: Vec2, context: string): Rect[] {
@@ -139,13 +143,14 @@ class Player extends Entity {
 
     powerup() {
 	playSound(SOUNDS['powerup']);
+	this.movespeed = 1+rnd(8);
+	this.firespeed = 2+rnd(10);
+	this.game.powerup();
     }
 }
 
 
 //  EnemyBase
-//  This class has the common methods for all enemies.
-//  They can be mixed in with applyMixins().
 //
 class EnemyBase extends Projectile {
 
@@ -199,7 +204,6 @@ class Enemy2 extends EnemyBase {
 
     update() {
 	super.update();
-	// Move wiggly vertically.
 	if (rnd(4) == 0) {
 	    this.movement.y = rnd(5)-2;
 	}
@@ -217,7 +221,7 @@ class Terrain extends TileMap {
 
     constructor(width: number, height: number) {
 	super(16, width, height);
-	this.isObstacle = ((c:number) => { return c != 0; });
+	this.isObstacle = ((c:number) => { return 0 < c; });
 	this.offset = 0;
 	this.cy = 0;
     }
@@ -258,22 +262,24 @@ class Terrain extends TileMap {
 // 
 class Game extends GameScene {
 
-    player: Player;
-    nextenemy: number;		// Enemy spawning counter.
-    score: number;
     scoreBox: TextBox;
+    player: Player;
     terrain: Terrain;
     stars: StarSprite;
+    
+    score: number;
     speed: number;
+    nextenemy: number;
 
     init() {
 	super.init();
 	this.scoreBox = new TextBox(this.screen.inflate(-8,-8), FONT);
 	this.player = new Player(this, this.screen.center());
-	this.player.chain(new DelayTask(2, () => { this.init(); }));
+	this.player.stopped.subscribe(() => { this.gameover(); });
 	this.add(this.player);
 	this.terrain = new Terrain(22, 15);
 	this.stars = new StarSprite(this.screen, 100);
+	
 	this.speed = -4;
 	this.nextenemy = 0;
 	this.score = 0;
@@ -284,14 +290,13 @@ class Game extends GameScene {
     update() {
 	super.update();
 	this.stars.move(new Vec2(this.speed, 0));
-	// Spawn an enemy at a random interval.
 	if (this.nextenemy == 0) {
 	    this.spawnEnemy();
 	    this.nextenemy = 10+rnd(20);
 	} else {
 	    this.nextenemy--;
 	}
-	this.terrain.proceed(4);
+	this.terrain.proceed(-this.speed);
     }
 
     onButtonPressed(keysym: KeySym) {
@@ -316,12 +321,34 @@ class Game extends GameScene {
 	    break;
 	}
 	this.add(enemy);
-	if (rnd(2) == 0) {
+	if (rnd(4) == 0) {
 	    let pos = new Vec2(this.screen.width, rnd(this.screen.height));
 	    this.add(new Thingy(this, pos));
 	}
     }
 
+    powerup() {
+	this.speed = -(1+rnd(8));
+	let banner = new BannerBox(this.screen, FONT, ['RANDOM POWERUP!!1']);
+	banner.lifetime = 1.5;
+	banner.interval = 0.3;
+	this.add(banner);
+    }
+
+    gameover() {
+	let banner = new BannerBox(this.screen, FONT, ['GAME OVER!!']);
+	banner.lifetime = 1.5;
+	banner.interval = 0.5;
+	this.add(banner);
+	this.add(new DelayTask(2, () => { this.init(); }));
+	var name = 'kitty';
+	var req = new XMLHttpRequest();
+	req.addEventListener('load', () => { this.showHighScores(); });
+	req.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+	req.open('POST', APIURL);
+	req.send('key='+APIKEY+'&name='+name+'&score='+this.score);
+    }	
+    
     addScore() {
 	this.score++;
 	this.updateScore();
@@ -330,6 +357,9 @@ class Game extends GameScene {
     updateScore() {
 	this.scoreBox.clear();
 	this.scoreBox.putText(['SCORE:'+format(this.score)]);
+    }
+
+    showHighScores() {
     }
 
     render(ctx: CanvasRenderingContext2D, bx: number, by: number) {
